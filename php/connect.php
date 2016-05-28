85e843de6c36c2b5a1e25efe67e464317fb7175f
Esteban <e_ramos@hotmail.es>
2016-05-28 01:04:33 -0300

    Add files via upload

--- php/connect.php
+++ php/connect.php
@@ -1,17 +1,20 @@
 <?php
   class Connection {
-      function recoverData($tableName) {
+      function connect() {
         $host = "127.0.0.1";
         $user = "root";
         $pass = "1234";
         $db_name = "CouchInnDB";
         $con = mysql_connect($host,$user,$pass) or die("Cannot connect to the db!");
         mysql_select_db($db_name,$con) or die("Database not found!");
+
+      }
+      function recoverData($tableName) {
         $query= "SELECT * FROM $tableName";
         $allData = mysql_query($query);
-        while ($fila = mysql_fetch_array($allData)) {
-          echo "$fila[descripcion] </br>";
-        }
+        $fila = mysql_fetch_array($allData);
+        return ($fila);
       }
+
   }
  ?>
