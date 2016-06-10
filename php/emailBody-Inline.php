a94d4cc58bfcc02ae0fb2dbe5c8561ddf9a8ee89
Laureano Lanatta <laureanolanatta@gmail.com>
2016-06-10 07:34:44 -0300

    Html Email Template
    Falta el link. Falta parametrizar. Falta testear.

---
+++ php/emailBody-Inline.php
@@ -0,0 +1,18 @@
+<div class="email-background" style="background-color: #eee;padding: 10px;">
+
+    <div class="pre-header" style="color: #eee;font-size: 1px;">
+        Bienvenido a CouchInn! Necesitas confirmar tu cuenta primero.
+    </div>
+
+    <div class="email-container" style="max-width: 500px;margin: 0 auto;font-family: sans-serif;overflow: hidden;">
+
+        <h1 style="text-align: center;color: #6D6D6D;">Confirmar tu cuenta</h1>
+        <img src="../images/resources/CouchInnLogoFull.png" style="max-width: 100%;">
+        <p style="margin: 20px;text-align: center;font-size: 18px;line-height: 1.5;color: #6D6D6D;font-weight: bold;">Antes de comenzar a utilizar tu cuenta, debes confirmar tu solicitud en el siguiente link.</p>
+        <div class="email-link" style="margin: 20px;text-align: center;">
+            <a href="#" style="text-decoration: none;display: inline-block;background: #96AC3C;color: #eee;font-weight: bold;padding: 15px 20px;border-radius: 5px;">Confirmar</a>
+        </div>
+
+    </div>
+
+</div>
---
+++ php/emailBody.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Email</title>
+    <link rel="stylesheet" href="../css/emailStyle.css" charset="utf-8">
+</head>
+<body>
+    <div class="email-background">
+        <div class="pre-header">
+            Bienvenido a CouchInn! Necesitas confirmar tu cuenta primero.
+        </div>
+        <div class="email-container">
+            <h1>Confirmar tu cuenta</h1>
+            <img src="../images/resources/CouchInnLogoFull.png" />
+            <p>Antes de comenzar a utilizar tu cuenta, debes confirmar tu solicitud en el siguiente link.</p>
+            <div class="email-link">
+                <a href="#">Confirmar</a>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
