af60ae75f676ff94c217294f45751eadf4c549ae
Nicolas <nico.mccormack@hotmail.com>
2016-06-15 20:50:26 -0300

    Add files via upload

---
+++ sql/dumps/tabla_couch.sql
@@ -0,0 +1,12 @@
+/*
+-- Query: select * from couch
+LIMIT 0, 1000
+
+-- Date: 2016-06-15 20:44
+*/
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (2,'mi casa','comoda',23,2,'1',6,1,1);
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (4,'Ofrezco mi cuarto','Todos los servicios disponibles',100,1,'1',230,6,1);
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (5,'Iglu: se alquila','Coqueto y espacioso',5,4,'1',666,8,1);
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (8,'Departamento ','Ofrezco mi dpto en zona centrica de la ciudad',150,3,'1',548,1,9);
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (9,'Bote','Ideal para conocer el lago de la ciudad',25,2,'1',995,7,10);
+INSERT INTO `couch` (`idcouch`,`titulo`,`descripcion`,`precio`,`capacidad`,`habilitado`,`idciudad`,`idtipo`,`idusuario`) VALUES (10,'Casa cercana a la costa','Ubicada a dos cuadras de la costanera norte',250,4,'0',875,5,9);
---
+++ sql/dumps/tabla_usuario.sql
@@ -0,0 +1,9 @@
+/*
+-- Query: select * from usuario
+LIMIT 0, 1000
+
+-- Date: 2016-06-15 20:47
+*/
+INSERT INTO `usuario` (`idusuario`,`email`,`nombre`,`apellido`,`password`,`sexo`,`telefono`,`calle`,`numero`,`idciudad`,`confirmado`,`nacimiento`,`foto_path`,`foto_nombre`) VALUES (1,'nico.mccormack@hotmail.com','nicolas','mc','$2y$10$IdCvekDsBJ90mIGhijJ8cOm867Y0Tzx7drgzD0L6jaDVEGSFBfL0S',NULL,NULL,NULL,NULL,12756,'1',NULL,NULL,NULL);
+INSERT INTO `usuario` (`idusuario`,`email`,`nombre`,`apellido`,`password`,`sexo`,`telefono`,`calle`,`numero`,`idciudad`,`confirmado`,`nacimiento`,`foto_path`,`foto_nombre`) VALUES (9,'zgtlyxfq@vomoto.com','gustavo','perez','$2y$10$9ks3QmNzCOWqahJ4GrT5Zub/hyNmjpgOJe/tUbZo2Pj.tFMQz3Cs2',NULL,NULL,NULL,NULL,90540,'1',NULL,NULL,NULL);
+INSERT INTO `usuario` (`idusuario`,`email`,`nombre`,`apellido`,`password`,`sexo`,`telefono`,`calle`,`numero`,`idciudad`,`confirmado`,`nacimiento`,`foto_path`,`foto_nombre`) VALUES (10,'nmc@gm.com','jose','mc','123','M',1,'1',1,12756,'1',NULL,NULL,NULL);
