4049f51679fd1a2bb7e42f9d1c56ffb22ef6868f
Laureano Lanatta <laureanolanatta@gmail.com>
2016-06-24 17:20:19 -0300

    Confirmation examples

--- js/confirmation.js
+++ js/confirmation.js
@@ -3,3 +3,15 @@ $(document).on("click", "#confirmEjemplo", function(e) {
       if (result) {window.location = $(document).find('#confirmEjemplo').attr('data-href');}
     });
 });
+
+$(document).on("click", "#deleteCouch", function(e) {
+    bootbox.confirm("Está seguro que desea eliminar el couch?", function(result) {
+      if (result) {window.location = $(document).find('#deleteCouch').attr('data-href');}
+    });
+});
+
+$(document).on("click", "#changeCouchState", function(e) {
+    bootbox.confirm("Está seguro que desea cambiar la visibilidad de este couch al publico?", function(result) {
+      if (result) {window.location = $(document).find('#changeCouchState').attr('data-href');}
+    });
+});
